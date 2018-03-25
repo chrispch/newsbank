7cad39ce7d8df479a470571ba2d128fe95b4e20c
chonghern <chonghern@gmail.com>
2018-03-25 19:09:26 +0800

    changing code to scrap from rss

--- crawler/scrap_news.py
+++ crawler/scrap_news.py
@@ -3,76 +3,138 @@ import json
 from bs4 import BeautifulSoup
 from datetime import datetime, time
 
-news_providers = {"TODAY": {
-                    "Singapore": "https://www.todayonline.com/api/v3/news_feed/3?items=8", 
-                    "World": "https://www.todayonline.com/api/v3/news_feed/5?items=8"
+news_providers = {  "TODAY": {
+                        "Hot News": "http://www.todayonline.com/hot-news/feed",
+                        "Singapore": "http://www.todayonline.com/feed/singapore", 
+                        "World": "http://www.todayonline.com/feed/world"
                     },
-                  "BBC": {
-                      "World": "http://www.bbc.com/news/world"
-                  }    
+                    "BBC": {
+                        "World": "http://feeds.bbci.co.uk/news/world/rss.xml"
+                    },
+                    "Guardian": {
+                        "World": "https://www.theguardian.com/world/rss"
+                    }
+
                 }
 api_url = "http://127.0.0.1:5000/"
 
+special_scrap_instructions = {  "TODAY": {
+                                    "Default": {
+                                        "full_text": {
+                                            "from_article":True,
+                                            "class":"article-detail_body"
+                                        },
+                                        "abstract": {
+                                            "from_article":True
+                                        },
+                                        "date_seconds": {
+                                            "from_article":False,
+                                            "class":"pubdate"
+                                        },
+                                        "author":{
+                                            "from_article":True
+                                        }
+                                    }
+                                },
+                                "BBC": {
+                                    "Default": {
+                                        "full_text": {
+                                            "from_article":True,
+                                            "class":"story-body__inner"
+                                        },
+                                        "abstract": {
+                                            "from_article":False,
+                                            "class":"div"
+                                        },
+                                        "date_seconds": {
+                                            "from_article":True,
+                                            "class":"date",
+                                            "attr":"data-seconds"
+                                        },
+                                        "author":{
+                                            "from_article":True
+                                        }
+                                    }
+                                },
+                                "Guardian": {
+                                    "Default": {
+                                        "full_text": {
+                                            "from_article":True,
+                                            "class":"content__article-body"
+                                        },
+                                        "abstract": {
+                                            "from_article":False,
+                                            "tag":"description"
+                                        },
+                                        "date_seconds": {
+                                            "from_article":False,
+                                            "class":"pubdate"
+                                        },
+                                        "author": {
+                                            "from_article":False,
+                                            "class":"dc:creator"
+                                        }
+                                    },
+                                }
+
+                            }
+
 def get_news(site, category):
     url = news_providers[site][category]
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    c = r.content.decode('utf-8')
-    if site == "TODAY":
-        # c = c.replace('\\u2019', '\'').replace('\\u2019', '\'').replace('\\', '')
-        news_list = json.loads(c, encoding='utf-8')
-        for news_item in news_list["nodes"]:
-            title = news_item["node"]["title"]
-            # get from API provider to check if article already exists in database
-            if requests.get(api_url + 'articles?where={"title":"' + title + '"}').json()["_items"] == []:
-                abstract = news_item["node"]["abstract"]
-                node_url = news_item["node"]["node_url"]
-                node_id = news_item["node"]["node_id"]
-                date = news_item["node"]["publication_date"]
-                dt = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-                date_seconds = dt.timestamp()
-                author = news_item["node"]["author"]
-                article = requests.get("https://www.todayonline.com/api/v3/article/{}?tid=3".format(node_id)).content.decode('utf-8')
-                full_text = json.loads(article, encoding='utf-8')["node"]["body"]
-                if author == []:
-                    author = "-"
-                else:
-                    author = author[0]["name"]
+    c = requests.get(url).content
+    soup = BeautifulSoup(c, "html.parser")
+    news_items = soup.find_all("item")
+    for news in news_items:
+        # get title
+        title = news.find("title").text
 
+        # get article link
+        href = news.find("link").text
+        if "https" not in href:
+            href = url + href
 
-    elif site == "BBC":
-        soup = BeautifulSoup(c, "html.parser")
-        # Buzzard
-        buzzard = soup.find("div", {"class": "buzzard-item"})
-        abstract = buzzard.find("p", {"class": "buzzard__summary"}).text
-        title = buzzard.find("a").text
-        data_seconds = float(buzzard.find("div", {"class": "date"})["data-seconds"])
-        href = "https://bbc.com" + buzzard.find("a").get("href")
-        # print(title, abstract, href, data_seconds)
-        # Pigeon
-        pigeon_items = soup.find("div", {"class": "pigeon"}).find_all("div", {"class": "pigeon-item"})
-        for item in pigeon_items:
-            body = item.find("div", {"class": "pigeon-item__body"})
-            if body:
-                abstract = body.find("p").text
-            else:
-                abstract = ""
-                body = item
-            title = body.find("a").text
-            href = "https://bbc.com" + body.find("a").get("href")
-            data_seconds = float(body.find("div", {"class": "date"})["data-seconds"])
-            # print(title, abstract, href, data_seconds)
-        # Macaw
-        macaw_items = soup.find("div", {"class": "macaw"}).find_all("div", {"class": "macaw-item"})
-        for item in macaw_items:
-            body = item.find("div", {"class": "macaw-item__body"})
-            title = body.find("a").text
-            abstract = ""
-            href = "https://bbc.com" + body.find("a").get("href")
-            data_seconds = float(body.find("div", {"class": "date"})["data-seconds"])
-            print(title, abstract, href, data_seconds)
+        # get article html
+        article = BeautifulSoup(requests.get(href).content, "html.parser")
+
+        # check if there are special scrap instructions for current category
+        if category not in special_scrap_instructions[site]:
+            i = special_scrap_instructions[site]["Default"]
+        else:
+            i = special_scrap_instructions[site][category]
+        
+        # get date_seconds
+        date = parse_instructions(i["date_seconds"], news, article)
+        print(date)
+        try:
+            dt = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
+        except ValueError:
+            dt = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %Z")
+        except ValueError:
+            dt = float(date)
+        date_seconds = dt.timestamp()
+        print(date_seconds)
+       
 
 def save_article(site, category, title, abstract, full_text, date_seconds, author, href):
-    
+    pass
+
+def parse_instructions(i, news_ref, article_ref):
+    if i["from_article"]:
+        _ref = article_ref
+    else:
+        _ref = news_ref
+    if "tag" in i and "class" in i:
+        search = '{}, \{"class":{}\}'.format(i["tag"], i["class"])
+    elif "tag" in i:
+        search = i["tag"]
+    elif "class" in i:
+        search = '"class": "{}" '.format(i["class"])
+    if "attr" in i:
+        return _ref.find(search)[i["attr"]]
+    else:
+        print(search)
+        return _ref.find(search)
 
 
-get_news("TODAY", "World")
+
+get_news("Guardian", "World")
