9fb20401820b51eeb27854a7829bb8ae880287e3
chonghern <chonghern@gmail.com>
2018-03-26 15:53:58 +0800

    now saves articles into mongodb

--- crawler/scrap_news.py
+++ crawler/scrap_news.py
@@ -3,37 +3,57 @@ import json
 import os
 from bs4 import BeautifulSoup
 from datetime import datetime, time
+from pymongo import MongoClient, DESCENDING, TEXT
 
 provider_json = os.path.dirname(os.path.realpath(__file__)) + "/news_providers.json"
 instructions_json = os.path.dirname(os.path.realpath(__file__)) + "/instructions.json"
+password_txt = os.path.dirname(os.path.realpath(__file__)) + "/password.txt"
 api_url = "http://127.0.0.1:5000/"
+database = "newsbank"
+collection = "articles"
 
-# load jsons
-with open(provider_json, "r") as f:
-    news_providers = json.load(f)
+# returns MongoClient collection
+def get_db(database, collection, username, password, port="27017", host="localhost"):
+    uri = "mongodb://{}:{}@{}:{}/{}".format(username, password, host, port, database)
+    client = MongoClient(uri)
+    db = client[database]
+    collection = db[collection]
+    return collection
 
-with open(instructions_json, "r") as f:
-    instructions = json.load(f)
+# create Mongo indexes
+def create_indexes(db):
+    # i = db.index_information()
+    # if 
+    # else:
+    db.create_index([("full_text", TEXT), ("title", TEXT)])
+    db.create_index([("date_seconds", DESCENDING)], unique=True)
+    print(db.index_information())
+            
 
-def get_news(site, category):
+# scraps news on a given site and category
+def get_news(site, category, db):
     url = news_providers[site][category]
     c = requests.get(url).content
     soup = BeautifulSoup(c, "xml")
-    # print(soup.prettify())
     news_items = soup.find_all("item")
     for news in news_items:
+        
         # get title
         title = news.find("title").text
-        print(title)
 
         # get article link
         href = news.find("link").text
         if "http" not in href:
             href = "http://" + href
-        print(href)
+        
+        # check if article already in db
+        if db.find_one({"href": href}) is not None:
+            # if article exists, all articles later than the current one already exist in db
+            break
 
-        # check if there are special scrap instructions for current category
+        # get scrapping instructions
         if category not in instructions[site]:
+            # check if there are special scrap instructions for current category
             i = instructions[site]["Default"]
         else:
             i = instructions[site][category]
@@ -46,7 +66,6 @@ def get_news(site, category):
         else:
             _href = i["article"]["base_url"].format(href)
             article = BeautifulSoup(requests.get(_href).content, "html.parser") 
-        print(_href)
         
         # get date_seconds
         date = parse_instructions(i["date_seconds"], news, article)
@@ -58,8 +77,6 @@ def get_news(site, category):
             date_seconds = dt.timestamp()
         except AttributeError:
             date_seconds = float(date)
-        
-        print(date_seconds)
 
         # get abstract
         if not i["article"]["cdn"]:
@@ -70,7 +87,6 @@ def get_news(site, category):
             abstract = abstract_soup.text
         else:
             abstract = abstract_soup.find("p").text
-        print(abstract)
 
         # get article text
         if not i["article"]["cdn"]:
@@ -85,17 +101,26 @@ def get_news(site, category):
         else:
             full_text = None
 
-        
-        print(full_text)
-
         # get author
         author = parse_instructions(i["author"], news, article)
         if author != None and not i["article"]["cdn"]:
             author = author.text
-        print(author)
+        
+        save_article(site, category, title, abstract, full_text, date_seconds, author, href, db)
 
-def save_article(site, category, title, abstract, full_text, date_seconds, author, href):
-    pass
+def save_article(site, category, title, abstract, full_text, date_seconds, author, href, db):
+    article = {
+        "site": site,
+        "category": category,
+        "title": title,
+        "abstract": abstract,
+        "full_text": full_text,
+        "date_seconds": date_seconds,
+        "author": author,
+        "href": href
+    }
+    # print(article)
+    db.insert_one(article)
 
 def parse_instructions(i, news_ref, article_ref):
     if i == None:
@@ -130,5 +155,25 @@ def parse_instructions(i, news_ref, article_ref):
         return results
 
 
+###################################################
+# Main
+###################################################
 
-get_news("BBC", "World")
+
+# load jsons
+with open(provider_json, "r") as f:
+    news_providers = json.load(f)
+
+with open(instructions_json, "r") as f:
+    instructions = json.load(f)
+
+# get mongo auth credentials
+with open (password_txt , 'r') as pw:
+    global username, password
+    read = pw.readline().rstrip().split(":")
+    username = read[0]
+    password = read[1]
+
+articles_db = get_db(database, collection, username, password)
+create_indexes(articles_db)
+get_news("BBC", "World", articles_db)
